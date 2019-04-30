5a4362096ea4ac1a99d0b8e4b546be8c528c5e32
Jakub Dolejs <jakubdolejs@gmail.com>
2019-04-30 13:01:18 +0100

    Updated plugin swift source to use new frameworks

--- src/ios/VerIDPlugin.swift
+++ src/ios/VerIDPlugin.swift
@@ -1,49 +1,58 @@
 import UIKit
-import VerID
+import VerIDCore
+import VerIDUI
 
-@objc(VerIDPlugin) public class VerIDPlugin: CDVPlugin, VerIDSessionDelegate {
+@objc(VerIDPlugin) public class VerIDPlugin: CDVPlugin, VerIDFactoryDelegate, VerIDSessionDelegate {
     
     private var veridSessionCallbackId: String?
+    private var verid: VerID?
     
     @objc public func load(_ command: CDVInvokedUrlCommand) {
-        self.loadVerID(command) {
+        self.loadVerID(command) { _ in
             self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
         }
     }
     
     @objc public func unload(_ command: CDVInvokedUrlCommand) {
-        VerID.shared.unload()
+        self.verid = nil
         self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
     }
     
     @objc public func registerUser(_ command: CDVInvokedUrlCommand) {
-        self.startSessionWithType(VerIDRegistrationSession.self, command: command)
+        do {
+            let settings: RegistrationSessionSettings = try self.createSettings(command.arguments)
+            self.startSession(command: command, settings: settings)
+        } catch {
+            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription), callbackId: command.callbackId)
+        }
     }
     
     @objc public func authenticate(_ command: CDVInvokedUrlCommand) {
-        self.startSessionWithType(VerIDAuthenticationSession.self, command: command)
+        do {
+            let settings: AuthenticationSessionSettings = try self.createSettings(command.arguments)
+            self.startSession(command: command, settings: settings)
+        } catch {
+            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription), callbackId: command.callbackId)
+        }
     }
     
     @objc public func captureLiveFace(_ command: CDVInvokedUrlCommand) {
-        self.startSessionWithType(VerIDLivenessDetectionSession.self, command: command)
+        do {
+            let settings: LivenessDetectionSessionSettings = try self.createSettings(command.arguments)
+            self.startSession(command: command, settings: settings)
+        } catch {
+            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription), callbackId: command.callbackId)
+        }
     }
     
     @objc public func getRegisteredUsers(_ command: CDVInvokedUrlCommand) {
-        commandDelegate.run {
+        self.loadVerID(command) { verid in
             var err: String = "Unknown error"
             do {
-                let veridUsers = try VerID.shared.registeredVerIDUsers()
-                var users: [String] = []
-                for user in veridUsers {
-                    if !users.contains(user.userId) {
-                        users.append(user.userId)
-                    }
-                }
+                let users = try verid.userManagement.users()
                 if let usersString = String(data: try JSONEncoder().encode(users), encoding: .utf8) {
-                    DispatchQueue.main.async {
-                        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: usersString)
-                        self.commandDelegate.send(result, callbackId: command.callbackId)
-                    }
+                    let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: usersString)
+                    self.commandDelegate.send(result, callbackId: command.callbackId)
                     return
                 } else {
                     err = "Failed to encode JSON as UTF-8 string"
@@ -51,18 +60,19 @@ import VerID
             } catch {
                 err = error.localizedDescription
             }
-            DispatchQueue.main.async {
-                self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: err), callbackId: command.callbackId)
-            }
+            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: err), callbackId: command.callbackId)
         }
     }
     
     @objc public func deleteUser(_ command: CDVInvokedUrlCommand) {
         if let userId = command.arguments?.compactMap({ ($0 as? [String:String])?["userId"] }).first {
-            commandDelegate.run {
-                VerID.shared.deregisterUser(userId)
-                let result = CDVPluginResult(status: CDVCommandStatus_OK)
-                DispatchQueue.main.async {
+            self.loadVerID(command) { verid in
+                verid.userManagement.deleteUsers([userId]) { error in
+                    if let err = error {
+                        self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: err.localizedDescription), callbackId: command.callbackId)
+                        return
+                    }
+                    let result = CDVPluginResult(status: CDVCommandStatus_OK)
                     self.commandDelegate.send(result, callbackId: command.callbackId)
                 }
             }
@@ -73,18 +83,20 @@ import VerID
 
     @objc public func compareFaceTemplates(_ command: CDVInvokedUrlCommand) {
         if let t1 = command.arguments?.compactMap({ ($0 as? [String:String])?["template1"] }).first?.data(using: .utf8), let t2 = command.arguments?.compactMap({ ($0 as? [String:String])?["template2"] }).first?.data(using: .utf8) {
-            commandDelegate.run {
-                do {
-                    let template1 = try JSONDecoder().decode(FaceTemplate.self, from: t1)
-                    let template2 = try JSONDecoder().decode(FaceTemplate.self, from: t2)
-                    let score = try FaceUtil.compareFaceTemplate(template1, to: template2).floatValue
-                    DispatchQueue.main.async {
-                        let message: [String:Any] = ["score":score];
-                        self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK, messageAs: message), callbackId: command.callbackId)
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription), callbackId: command.callbackId)
+            self.loadVerID(command) { verid in
+                self.commandDelegate.run {
+                    do {
+                        let template1 = try JSONDecoder().decode(RecognitionFace.self, from: t1)
+                        let template2 = try JSONDecoder().decode(RecognitionFace.self, from: t2)
+                        let score = try verid.faceRecognition.compareSubjectFaces([template1], toFaces: [template2]).floatValue
+                        DispatchQueue.main.async {
+                            let message: [String:Any] = ["score":score,"threshold":verid.faceRecognition.authenticationScoreThreshold.floatValue];
+                            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK, messageAs: message), callbackId: command.callbackId)
+                        }
+                    } catch {
+                        DispatchQueue.main.async {
+                            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription), callbackId: command.callbackId)
+                        }
                     }
                 }
             }
@@ -96,7 +108,7 @@ import VerID
     }
     
     @objc public func detectFaceInImage(_ command: CDVInvokedUrlCommand) {
-        self.loadVerID(command) {
+        self.loadVerID(command) { verid in
             self.commandDelegate.run(inBackground: {
                 do {
                     guard let imageString = command.arguments?.compactMap({ ($0 as? [String:String])?["image"] }).first else {
@@ -112,11 +124,37 @@ import VerID
                     guard let data = Data(base64Encoded: String(imageString[dataIndex...])) else {
                         throw VerIDPluginError.invalidArgument
                     }
-                    guard let image = UIImage(data: data) else {
+                    guard let image = UIImage(data: data), let cgImage = image.cgImage else {
                         throw VerIDPluginError.invalidArgument
                     }
-                    let face = try VerID.shared.detectFaceInImage(image, keepForRecognition: true)
-                    guard let encodedFace = String(data: try JSONEncoder().encode(face), encoding: .utf8) else {
+                    let orientation: CGImagePropertyOrientation
+                    switch image.imageOrientation {
+                    case .up:
+                        orientation = .up
+                    case .down:
+                        orientation = .down
+                    case .left:
+                        orientation = .left
+                    case .right:
+                        orientation = .right
+                    case .upMirrored:
+                        orientation = .upMirrored
+                    case .downMirrored:
+                        orientation = .downMirrored
+                    case .leftMirrored:
+                        orientation = .leftMirrored
+                    case .rightMirrored:
+                        orientation = .rightMirrored
+                    @unknown default:
+                        orientation = .up
+                    }
+                    let veridImage = VerIDImage(cgImage: cgImage, orientation: orientation)
+                    let faces = try verid.faceDetection.detectFacesInImage(veridImage, limit: 1, options: 0)
+                    guard let recognizableFace = try verid.faceRecognition.createRecognizableFacesFromFaces(faces, inImage: veridImage).first else {
+                        throw VerIDPluginError.faceTemplateExtractionError
+                    }
+                    let recognitionFace = RecognitionFace(recognitionData: recognizableFace.recognitionData)
+                    guard let encodedFace = String(data: try JSONEncoder().encode(recognitionFace), encoding: .utf8) else {
                         throw VerIDPluginError.encodingError
                     }
                     DispatchQueue.main.async {
@@ -158,6 +196,10 @@ import VerID
         }
     }
     
+    public func sessionWasCanceled(_ session: VerIDSession) {
+        self.veridSessionCallbackId = nil
+    }
+    
     
     // MARK: - Session helpers
     
@@ -173,65 +215,58 @@ import VerID
     
     private func defaultSettings<T: VerIDSessionSettings>() -> T {
         switch T.self {
-        case is VerIDRegistrationSessionSettings.Type:
-            return VerIDRegistrationSessionSettings() as! T
-        case is VerIDAuthenticationSessionSettings.Type:
-            return VerIDAuthenticationSessionSettings() as! T
-        case is VerIDLivenessDetectionSessionSettings.Type:
-            return VerIDLivenessDetectionSessionSettings() as! T
+        case is RegistrationSessionSettings.Type:
+            return RegistrationSessionSettings(userId: "default") as! T
+        case is AuthenticationSessionSettings.Type:
+            return AuthenticationSessionSettings(userId: "default") as! T
+        case is LivenessDetectionSessionSettings.Type:
+            return LivenessDetectionSessionSettings() as! T
         default:
             return VerIDSessionSettings() as! T
         }
     }
     
-    private func startSessionWithType<T: VerIDSession>(_ type: T.Type, command: CDVInvokedUrlCommand) {
+    private func startSession<T: VerIDSessionSettings>(command: CDVInvokedUrlCommand, settings: T) {
         guard self.veridSessionCallbackId == nil || self.veridSessionCallbackId!.isEmpty else {
             self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR), callbackId: command.callbackId)
             return
         }
-        self.loadVerID(command) {
-            let veridSession: VerIDSession
-            do {
-                switch type {
-                case is VerIDRegistrationSession.Type:
-                    let regSessionSettings: VerIDRegistrationSessionSettings = try self.createSettings(command.arguments)
-                    veridSession = VerIDRegistrationSession(settings: regSessionSettings)
-                case is VerIDAuthenticationSession.Type:
-                    let authSessionSettings: VerIDAuthenticationSessionSettings = try self.createSettings(command.arguments)
-                    veridSession = VerIDAuthenticationSession(settings: authSessionSettings)
-                case is VerIDLivenessDetectionSession.Type:
-                    let livenessSessionSettings: VerIDLivenessDetectionSessionSettings = try self.createSettings(command.arguments)
-                    veridSession = VerIDLivenessDetectionSession(settings: livenessSessionSettings)
-                default:
-                    veridSession = VerIDSession()
-                }
-            } catch {
-                self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription), callbackId: command.callbackId)
-                return
-            }
+        self.loadVerID(command) { verid in
             self.veridSessionCallbackId = command.callbackId
-            veridSession.delegate = self
-            veridSession.start()
+            let session = VerIDSession(environment: verid, settings: settings)
+            session.delegate = self
+            session.start()
         }
     }
     
-    func loadVerID(_ command: CDVInvokedUrlCommand, callback: @escaping () -> Void) {
-        let apiSecret = command.arguments?.compactMap({ ($0 as? [String:String])?["apiSecret"] }).first
-        if !VerID.shared.isLoaded {
-            VerID.shared.load(apiSecret) { (success, error) in
-                if error == nil && success {
-                    callback()
-                } else {
-                    let err = error?.localizedDescription ?? "Unknown error"
-                    self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: err), callbackId: command.callbackId)
-                }
-            }
-        } else {
-            callback()
+    func loadVerID(_ command: CDVInvokedUrlCommand, callback: @escaping (VerID) -> Void) {
+        if let verid = self.verid {
+            callback(verid)
+            return
+        }
+        self.veridSessionCallbackId = command.callbackId
+        let veridFactory = VerIDFactory()
+        if let apiSecret = command.arguments?.compactMap({ ($0 as? [String:String])?["apiSecret"] }).first {
+            let detRecLibFactory = VerIDFaceDetectionRecognitionFactory(apiSecret: apiSecret)
+            veridFactory.faceDetectionFactory = detRecLibFactory
+            veridFactory.faceRecognitionFactory = detRecLibFactory
+        }
+        veridFactory.delegate = self
+        veridFactory.createVerID { verid in
+            self.veridSessionCallbackId = nil
+            self.verid = verid
+            callback(verid)
+        }
+    }
+    
+    public func veridFactory(_ factory: VerIDFactory, didFailWithError error: Error) {
+        if let callbackId = self.veridSessionCallbackId {
+            self.veridSessionCallbackId = nil
+            self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription), callbackId: callbackId)
         }
     }
 }
 
 public enum VerIDPluginError: Int, Error {
-    case parsingError, invalidArgument, encodingError
+    case parsingError, invalidArgument, encodingError, faceTemplateExtractionError
 }
