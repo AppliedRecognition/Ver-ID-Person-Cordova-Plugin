2b88196b6cb2571cbfc8ed0b251fe76a6fd5105d
Jakub Dolejs <jakubdolejs@gmail.com>
2019-05-02 20:03:39 +0100

    Corrected compareFaces method signature

--- www/dist/Ver-ID.js
+++ www/dist/Ver-ID.js
@@ -144,7 +144,7 @@ var VerID = (function () {
     };
     VerID.prototype.compareFaces = function (face1, face2) {
         return new Promise(function (resolve, reject) {
-            cordova.exec(decodeResult(resolve), reject, PLUGIN_NAME, "compareFace", [{ "face1": face1 }, { "face2": face2 }]);
+            cordova.exec(decodeResult(resolve), reject, PLUGIN_NAME, "compareFaces", [{ "face1": face1 }, { "face2": face2 }]);
         });
     };
     VerID.prototype.detectFaceInImage = function (image) {
