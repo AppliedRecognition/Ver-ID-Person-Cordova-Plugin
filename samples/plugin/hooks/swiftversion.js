3350071991a176a37d4516f35ff40f0d95e915bd
Jonathan Matus Moreno <jmatus@qxdev.com>
2020-06-23 17:56:49 -0600

    Revert "Removed samples and samples documentation"
    This reverts commit 6322802d184490f78384209419753229cb36a5a5.

---
+++ samples/plugin/hooks/swiftversion.js
@@ -0,0 +1,35 @@
+module.exports = function(context) {
+    if (context.opts.cordova.platforms.indexOf("ios") < 0) {
+        return;
+    }
+    console.log("Running iOS Swift version check for Ver-ID plugin");
+    var deferral = context.requireCordovaModule("q").defer();
+    var xcProject = require("./xcodeproject.js")(context);
+    xcProject.readProjectFile().then(function(data) {
+        return new Promise(function(resolve,reject) {
+            var incompatibleSwiftVersion = false;
+            data = data.replace(/buildSettings = \{((.|\s)*?)\};/g, function(match) {
+                var swiftPattern = /SWIFT_VERSION\s*=\s*[\'\"]*(\d+)(\.(\d+))*[\'\"]*;/;
+                var swift = match.match(swiftPattern);
+                var result = match;
+                if (swift) {
+                    if ((parseInt(swift[1]) < 4) || (swift[1] == "4" && swift[3] && parseInt(swift[3]) < 2)) {
+                        // result = match.replace(swiftPattern, "SWIFT_VERSION = 4.2;");
+                        incompatibleSwiftVersion = swift[1]+swift[2];
+                    }
+                } else {
+                    result = match.replace(/\};$/, "SWIFT_VERSION = 4.2;\n\t\t\t};");
+                }
+                return result;
+            });
+            if (incompatibleSwiftVersion) {
+                console.log("Incompatible Swift version: "+incompatibleSwiftVersion);
+                return reject();
+            }
+            return resolve(data);
+        });
+    }).then(function(data){
+        return xcProject.writeProjectFile(data);
+    }).then(deferral.resolve).catch(deferral.reject);
+    return deferral.promise;
+};
